c7ff285e670d5c04cc49e03ae25c793a2c9674da
吴玉铁 <Ladn@dingjunxiaodeMacBook-Pro.local>
2015-09-13 19:47:47 +0800

    Initial Commit

---
+++ 杭电神器/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  杭电神器
+//
+//  Created by 吴玉铁 on 15/9/13.
+//  Copyright (c) 2015年 铁哥. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 杭电神器/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  杭电神器
+//
+//  Created by 吴玉铁 on 15/9/13.
+//  Copyright (c) 2015年 铁哥. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
